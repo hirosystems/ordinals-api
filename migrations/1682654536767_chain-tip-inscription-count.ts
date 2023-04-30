191b155f5aca433aa488fe2b45a7bda61f6d305f
Rafael Cardenas <rafael@rafaelcr.com>
2023-04-30 13:53:14 -0600

    chore: add migration

---
+++ migrations/1682654536767_chain-tip-inscription-count.ts
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { MigrationBuilder, ColumnDefinitions } from 'node-pg-migrate';
+
+export const shorthands: ColumnDefinitions | undefined = undefined;
+
+export function up(pgm: MigrationBuilder): void {
+  pgm.addColumn('chain_tip', {
+    inscription_count: {
+      type: 'int',
+      notNull: true,
+      default: 0,
+    },
+  });
+}
