4265853b82f617528e27156ee20b4105fe3e915e
Rafael Cardenas <rafael@rafaelcr.com>
2023-05-02 15:12:00 -0600

    chore: add materialized view migrations

---
+++ migrations/1683047918926_mime-type-counts.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { MigrationBuilder, ColumnDefinitions } from 'node-pg-migrate';
+
+export const shorthands: ColumnDefinitions | undefined = undefined;
+
+export function up(pgm: MigrationBuilder): void {
+  pgm.createMaterializedView(
+    'mime_type_counts',
+    { data: true },
+    `SELECT mime_type, COUNT(*) AS count FROM inscriptions GROUP BY mime_type`
+  );
+}
