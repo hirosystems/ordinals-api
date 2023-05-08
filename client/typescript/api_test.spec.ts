e41c985a3c462b28fb666a390dace2564193e0fd
Rafael Cardenas <rafael@rafaelcr.com>
2023-05-08 16:13:47 -0600

    fix: manual tests

--- client/typescript/api_test.spec.ts
+++ client/typescript/api_test.spec.ts
@@ -22,21 +22,21 @@ describe("InscriptionsApi", () => {
   });
 
   test("getInscription", () => {
-    const id: api.InscriptionIdentifier = undefined
+    const id: string = "id_example"
     return expect(instance.getInscription(id, {})).resolves.toBe(null)
   })
   test("getInscriptionContent", () => {
-    const id: api.InscriptionIdentifier1 = undefined
+    const id: string = "id_example"
     return expect(instance.getInscriptionContent(id, {})).resolves.toBe(null)
   })
   test("getInscriptionTransfers", () => {
-    const id: api.InscriptionIdentifier2 = undefined
+    const id: string = "id_example"
     const offset: number = 56
     const limit: number = 56
     return expect(instance.getInscriptionTransfers(id, offset, limit, {})).resolves.toBe(null)
   })
   test("getInscriptions", () => {
-    const genesis_block: api.BlockIdentifier = undefined
+    const genesis_block: string = "genesis_block_example"
     const from_genesis_block_height: string = "from_genesis_block_height_example"
     const to_genesis_block_height: string = "to_genesis_block_height_example"
     const from_genesis_timestamp: number = 56
@@ -47,16 +47,16 @@ describe("InscriptionsApi", () => {
     const to_sat_coinbase_height: string = "to_sat_coinbase_height_example"
     const from_number: number = 56
     const to_number: number = 56
-    const id: Array<string> = undefined
-    const number: Array<number> = undefined
+    const id: Array<string> = ["id_example"]
+    const number: Array<number> = [56]
     const output: string = "output_example"
-    const address: Array<string> = undefined
-    const mime_type: Array<string> = undefined
-    const rarity: Array<api.string> = undefined
+    const address: Array<string> = ["address_example"]
+    const mime_type: Array<string> = ["mime_type_example"]
+    const rarity: Array<string> = ["rarity_example"]
     const offset: number = 56
     const limit: number = 56
-    const order_by: api.OrderBy = undefined
-    const order: api.Order = undefined
+    const order_by: string = "order_by_example"
+    const order: string = "order_example"
     return expect(instance.getInscriptions(genesis_block, from_genesis_block_height, to_genesis_block_height, from_genesis_timestamp, to_genesis_timestamp, from_sat_ordinal, to_sat_ordinal, from_sat_coinbase_height, to_sat_coinbase_height, from_number, to_number, id, number, output, address, mime_type, rarity, offset, limit, order_by, order, {})).resolves.toBe(null)
   })
 })
