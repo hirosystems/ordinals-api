4eb53cc37a68c23f2a713e83e0fef7dd160e6c03
Rafael Cardenas <rafael@rafaelcr.com>
2023-05-12 10:40:20 -0600

    docs: comment on chain tip default block height [skip ci]

--- migrations/1683130430977_chain-tip-view.ts
+++ migrations/1683130430977_chain-tip-view.ts
@@ -8,6 +8,7 @@ export function up(pgm: MigrationBuilder): void {
   pgm.createMaterializedView(
     'chain_tip',
     { data: true },
+    // Set block height 767430 (inscription #0 genesis) as default.
     `SELECT GREATEST(MAX(block_height), 767430) AS block_height FROM locations`
   );
 }
