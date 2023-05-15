60687dbd78509f0c1e9273e65edf7307824e1cf9
Rafael Cardenas <rafael@rafaelcr.com>
2023-05-15 13:54:30 -0600

    chore: add brc20 migrations

---
+++ migrations/1684175810998_brc20-balances.ts
@@ -0,0 +1,31 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { MigrationBuilder, ColumnDefinitions } from 'node-pg-migrate';
+
+export const shorthands: ColumnDefinitions | undefined = undefined;
+
+export function up(pgm: MigrationBuilder): void {
+  pgm.createTable('brc20_balances', {
+    id: {
+      type: 'bigserial',
+      primaryKey: true,
+    },
+    brc20_deploy_id: {
+      type: 'bigint',
+      notNull: true,
+    },
+    address: {
+      type: 'text',
+      notNull: true,
+    },
+    balance: {
+      type: 'numeric',
+      notNull: true,
+    },
+  });
+  pgm.createConstraint(
+    'brc20_balances',
+    'brc20_balances_brc20_deploy_id_fk',
+    'FOREIGN KEY(brc20_deploy_id) REFERENCES brc20_deploys(id) ON DELETE CASCADE'
+  );
+  pgm.createIndex('brc20_balances', ['address']);
+}
