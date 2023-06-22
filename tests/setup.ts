ed56203e3f16307adf82d853323c2fabbdec08d0
Rafael Cardenas <rafael@rafaelcr.com>
2023-06-21 22:10:22 -0600

    fix: use chainhook client to create event server

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,5 +1,6 @@
 // ts-unused-exports:disable-next-line
 export default (): void => {
   process.env.CHAINHOOK_NODE_AUTH_TOKEN = 'test';
+  process.env.CHAINHOOK_NODE_RPC_HOST = 'test.chainhooks.com';
   process.env.PGDATABASE = 'postgres';
 };
