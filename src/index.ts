23c7e3cabe889f0ad152356e79027671b2c572e3
Rafael Cardenas <rafael@rafaelcr.com>
2023-06-28 14:03:06 -0600

    feat: api ingestion metrics on prometheus

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { isProdEnv } from './api/util/helpers';
 import { buildChainhookServer } from './chainhook/server';
 import { ENV } from './env';
 import { logger } from './logger';
+import { ApiMetrics } from './metrics/metrics';
 import { PgStore } from './pg/pg-store';
 import { registerShutdownConfig } from './shutdown-handler';
 
@@ -43,6 +44,7 @@ async function initApiService(db: PgStore) {
       },
     });
 
+    ApiMetrics.configure(db);
     await promServer.listen({ host: ENV.API_HOST, port: 9153 });
   }
 }
