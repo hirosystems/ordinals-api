e63934319049bf950c58ef3bc6ba0ebb0b65b9c2
Rafael Cárdenas <rafael@rafaelcr.com>
2023-06-30 13:30:22 -0600

    fix: throw error on invalid chainhook payloads (#117)

--- src/chainhook/helpers.ts
+++ src/chainhook/helpers.ts
@@ -11,7 +11,7 @@ export async function processInscriptionFeed(payload: unknown, db: PgStore): Pro
   if (!ChainhookPayloadCType.Check(payload)) {
     const errors = [...ChainhookPayloadCType.Errors(payload)];
     logger.error(errors, `[inscription_feed] invalid payload`);
-    return;
+    throw new Error(`Unable to process invalid chainhook payload`);
   }
   await db.updateInscriptions(payload);
 }
