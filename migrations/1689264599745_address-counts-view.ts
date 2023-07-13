c73a97185a28904fa65711078340f6d2c262babd
Rafael Cardenas <rafael@rafaelcr.com>
2023-07-13 11:02:51 -0600

    fix: make index unique

--- migrations/1689264599745_address-counts-view.ts
+++ migrations/1689264599745_address-counts-view.ts
@@ -6,11 +6,11 @@ export const shorthands: ColumnDefinitions | undefined = undefined;
 export function up(pgm: MigrationBuilder): void {
   pgm.createMaterializedView(
     'address_counts',
-    {},
+    { data: true },
     `SELECT l.address, COUNT(*) AS count
     FROM current AS c
     INNER JOIN locations AS l ON c.location_id = l.id
     GROUP BY l.address`
   );
-  pgm.createIndex('address_counts', ['address']);
+  pgm.createIndex('address_counts', ['address'], { unique: true });
 }
