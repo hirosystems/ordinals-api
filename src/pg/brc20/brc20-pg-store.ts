7e154bed7a1e8e532f7bd62be7beb2bf25b88905
Rafael Cardenas <rafael@rafaelcr.com>
2023-08-30 12:24:10 -0600

    fix: validate mint data in pg

--- src/pg/brc20/brc20-pg-store.ts
+++ src/pg/brc20/brc20-pg-store.ts
@@ -298,34 +298,42 @@ export class Brc20PgStore extends BasePgStoreModule {
   private async insertMint(mint: { op: Brc20Mint; location: DbBrc20Location }): Promise<void> {
     await this.sqlWriteTransaction(async sql => {
       if (!mint.location.inscription_id || !mint.location.address) return;
-      const tokenRes = await sql<
-        { id: string; decimals: string; limit: string; max: string; minted_supply: string }[]
-      >`
+      const tokenRes = await sql<{ brc20_deploy_id: string; real_mint_amount: string }[]>`
+        WITH mint_data AS (
+          SELECT
+            d.id, d.decimals, d.limit, d.max,
+            COALESCE(SUM(amount), 0) AS minted_supply
+          FROM brc20_deploys AS d
+          LEFT JOIN brc20_mints AS m ON m.brc20_deploy_id = d.id
+          WHERE d.ticker_lower = LOWER(${mint.op.tick})
+          GROUP BY d.id
+        )
         SELECT
-          d.id, d.decimals, d.limit, d.max,
-          COALESCE(SUM(amount), 0) AS minted_supply
-        FROM brc20_deploys AS d
-        LEFT JOIN brc20_mints AS m ON m.brc20_deploy_id = d.id
-        WHERE d.ticker_lower = LOWER(${mint.op.tick})
-        GROUP BY d.id
+          m.id AS brc20_deploy_id,
+          LEAST(${mint.op.amt}, m.max - m.minted_supply) AS real_mint_amount
+        FROM mint_data AS m
+        WHERE
+          (m.minted_supply < m.max)
+          AND (m.limit IS NULL OR ${mint.op.amt} <= m.limit)
+          AND (SCALE(${mint.op.amt}) <= m.decimals)
       `;
       if (tokenRes.count === 0) return;
       const token = tokenRes[0];
 
       // Is the mint amount within the allowed token limits?
-      if (token.limit && BigNumber(mint.op.amt).isGreaterThan(token.limit)) return;
+      // if (token.limit && BigNumber(mint.op.amt).isGreaterThan(token.limit)) return;
       // Is the number of decimals correct?
-      if (mint.op.amt.includes('.') && mint.op.amt.split('.')[1].length > parseInt(token.decimals))
-        return;
+      // if (mint.op.amt.includes('.') && mint.op.amt.split('.')[1].length > parseInt(token.decimals))
+      //   return;
       // Does the mint amount exceed remaining supply?
-      const minted = new BigNumber(token.minted_supply);
-      const availSupply = new BigNumber(token.max).minus(minted);
-      if (availSupply.isLessThanOrEqualTo(0)) return;
-      const mintAmt = BigNumber.min(availSupply, mint.op.amt);
+      // const minted = new BigNumber(token.minted_supply);
+      // const availSupply = new BigNumber(token.max).minus(minted);
+      // if (availSupply.isLessThanOrEqualTo(0)) return;
+      // const mintAmt = BigNumber.min(availSupply, mint.op.amt);
 
       const mintInsert: DbBrc20MintInsert = {
         inscription_id: mint.location.inscription_id,
-        brc20_deploy_id: token.id,
+        brc20_deploy_id: token.brc20_deploy_id,
         block_height: mint.location.block_height,
         tx_id: mint.location.tx_id,
         address: mint.location.address,
@@ -334,9 +342,9 @@ export class Brc20PgStore extends BasePgStoreModule {
       const balanceInsert: DbBrc20BalanceInsert = {
         inscription_id: mint.location.inscription_id,
         location_id: mint.location.id,
-        brc20_deploy_id: token.id,
+        brc20_deploy_id: token.brc20_deploy_id,
         address: mint.location.address,
-        avail_balance: mintAmt.toString(),
+        avail_balance: token.real_mint_amount,
         trans_balance: '0',
         type: DbBrc20BalanceTypeId.mint,
       };
