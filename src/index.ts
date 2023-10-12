108a45de66321682f28ff91d16d4e2780d6f360b
Rafael Cardenas <rafael@rafaelcr.com>
2023-10-12 09:17:20 -0600

    chore: switch to p-queue

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,17 @@
 import { isProdEnv, logger, registerShutdownConfig } from '@hirosystems/api-toolkit';
 import { buildApiServer, buildPromServer } from './api/init';
-import { startChainhookServer } from './chainhook/server';
 import { ENV } from './env';
 import { ApiMetrics } from './metrics/metrics';
 import { PgStore } from './pg/pg-store';
-import { buildAdminRpcServer } from './admin-rpc/init';
 import { buildOrdhookIndexer } from './ordhook';
-import Queue from 'queue';
 
 function numberRange(start: number, end: number) {
   return Array.from(Array(end - start + 1).keys()).map(x => x + start);
 }
 
-async function initBackgroundServices(jobQueue: Queue, db: PgStore) {
+async function initBackgroundServices(db: PgStore) {
   logger.info('Initializing background services...');
-  const ordhook = buildOrdhookIndexer(jobQueue, db);
+  const ordhook = buildOrdhookIndexer(db);
   registerShutdownConfig({
     name: 'Ordhook Indexer',
     forceKillable: false,
@@ -58,13 +55,8 @@ async function initApp() {
   logger.info(`Initializing in ${ENV.RUN_MODE} run mode...`);
   const db = await PgStore.connect({ skipMigrations: ENV.RUN_MODE === 'readonly' });
 
-  const jobQueue = new Queue({ 
-    concurrency: 1,
-    autostart: true,
-  });
-
   if (['default', 'writeonly'].includes(ENV.RUN_MODE)) {
-    await initBackgroundServices(jobQueue, db);
+    await initBackgroundServices(db);
   }
   if (['default', 'readonly'].includes(ENV.RUN_MODE)) {
     await initApiService(db);
--- src/ordhook/index.ts
+++ src/ordhook/index.ts
@@ -2,13 +2,13 @@ import { OrdinalsIndexer } from '@hirosystems/ordhook-sdk-js';
 import { ENV } from '../env';
 import { PgStore } from '../pg/pg-store';
 import { BitcoinEvent } from '@hirosystems/chainhook-client';
-import Queue from 'queue';
+import PQueue from 'p-queue';
 
 export interface OrdhookBlock {
   block: BitcoinEvent;
 }
 
-export function buildOrdhookIndexer(jobQueue: Queue, db: PgStore): OrdinalsIndexer {
+export function buildOrdhookIndexer(db: PgStore): OrdinalsIndexer {
   const ordhook = new OrdinalsIndexer({
     bitcoinRpcUrl: ENV.BITCOIN_RPC_URL,
     bitcoinRpcUsername: ENV.BITCOIN_RPC_USERNAME,
@@ -16,33 +16,40 @@ export function buildOrdhookIndexer(jobQueue: Queue, db: PgStore): OrdinalsIndex
     workingDirectory: ENV.ORDHOOK_WORKING_DIR,
     logs: true,
   });
+  const jobQueue = new PQueue({
+    concurrency: 1,
+    autoStart: true,
+  });
+
   ordhook.onBlock(block => {
-    console.log(`Queue size: ${jobQueue.length}`);
+    console.log(`Queue size: ${jobQueue.size}`);
 
     // Early return: if the queue is full, reject the block
-    if (jobQueue.length > 10) {
+    // TODO: Do this using PQueue's limits
+    if (jobQueue.size > 10) {
       console.log('Blocking');
       return false;
     }
 
     // Enqueue
-    jobQueue.push(async () => {
+    void jobQueue.add(async () => {
       await db.insertBlock(block as OrdhookBlock);
     });
     return true;
   });
 
   ordhook.onBlockRollBack(block => {
-    console.log(`Queue size: ${jobQueue.length}`);
+    console.log(`Queue size: ${jobQueue.size}`);
 
     // Early return: if the queue is full, reject the block
-    if (jobQueue.length > 10) {
+    // TODO: Do this using PQueue's limits
+    if (jobQueue.size > 10) {
       console.log('Blocking');
       return false;
     }
 
     // Enqueue
-    jobQueue.push(async () => {
+    void jobQueue.add(async () => {
       await db.rollBackBlock(block as OrdhookBlock);
     });
     return true;
