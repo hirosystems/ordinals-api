c6ed3da1f001855e305769d62f448489207ffab9
Rafael Cardenas <rafael@rafaelcr.com>
2023-11-01 19:56:29 -0600

    fix: status tests

--- tests/status.test.ts
+++ tests/status.test.ts
@@ -41,7 +41,7 @@ describe('Status', () => {
           content_bytes: '0x48656C6C6F',
           content_type: 'text/plain;charset=utf-8',
           content_length: 5,
-          inscription_number: 7,
+          inscription_number: 0,
           inscription_fee: 2805,
           inscription_id: '38c46a8bf7ec90bc7f6b797e7dc84baa97f4e5fd4286b92fe1b50176d03b18dci0',
           inscription_output_value: 10000,
@@ -90,7 +90,7 @@ describe('Status', () => {
       server_version: 'ordinals-api v0.0.1 (test:123456)',
       status: 'ready',
       block_height: 791975,
-      max_inscription_number: 7,
+      max_inscription_number: 0,
       max_cursed_inscription_number: -2,
     });
   });
