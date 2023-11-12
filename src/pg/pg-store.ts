e2ed039120958fde07297c6760022770dab710a8
Rafael Cardenas <rafael@rafaelcr.com>
2023-11-11 21:05:59 -0600

    fix: try batch iterator generator

--- src/pg/pg-store.ts
+++ src/pg/pg-store.ts
@@ -3,6 +3,7 @@ import {
   PgConnectionVars,
   PgSqlClient,
   PgSqlQuery,
+  batchIterate,
   connectPostgres,
   logger,
   runMigrations,
@@ -255,7 +256,7 @@ export class PgStore extends BasePgStore {
         });
         // Divide insertion array into chunks of 3000 in order to avoid the postgres limit of 65534
         // query params.
-        for (const writeChunk of chunkArray(writes, 3000))
+        for (const writeChunk of batchIterate(writes, 3000))
           await this.insertInscriptions(writeChunk);
         updatedBlockHeightMin = Math.min(updatedBlockHeightMin, event.block_identifier.index);
         if (ENV.BRC20_BLOCK_SCAN_ENABLED)
