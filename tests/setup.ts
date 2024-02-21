67ea70c361e28647ce270ab3171de7b5dac53e72
Rafael Cardenas <rafael@rafaelcr.com>
2024-02-21 13:47:07 -0600

    fix: test setup

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,6 +1,7 @@
 // ts-unused-exports:disable-next-line
 export default (): void => {
-  process.env.CHAINHOOK_NODE_AUTH_TOKEN = 'test';
-  process.env.CHAINHOOK_NODE_RPC_HOST = 'test.chainhooks.com';
+  process.env.ORDHOOK_NODE_AUTH_TOKEN = 'test';
+  process.env.ORDHOOK_NODE_RPC_HOST = 'test.chainhooks.com';
+  process.env.ORDHOOK_NODE_RPC_PORT = '13370';
   process.env.PGDATABASE = 'postgres';
 };
