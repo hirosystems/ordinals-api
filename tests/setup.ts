72fd3fda24919f14218cbbf02a552dfcf4d8ea52
Rafael Cárdenas <rafael@rafaelcr.com>
2024-02-21 15:48:00 -0600

    feat: shutdown gracefully after finishing replaying blocks (#315)
    * feat: shutdown gracefully after finishing replaying blocks
    
    * test: shutdown

--- tests/setup.ts
+++ tests/setup.ts
@@ -3,5 +3,6 @@ export default (): void => {
   process.env.ORDHOOK_NODE_AUTH_TOKEN = 'test';
   process.env.ORDHOOK_NODE_RPC_HOST = 'test.chainhooks.com';
   process.env.ORDHOOK_NODE_RPC_PORT = '13370';
+  process.env.ORDHOOK_INGESTION_MODE = 'default';
   process.env.PGDATABASE = 'postgres';
 };
